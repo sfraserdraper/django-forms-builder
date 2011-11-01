e3dd750e947783086471a7fd72cde38d72fdb466
Stephen Fraser <steve@needle.com>
2011-11-01 16:55:07 -0600

    Fix unit tests so they pass again.  [Fixes: #20396099]

--- forms_builder/forms/tests.py
+++ forms_builder/forms/tests.py
@@ -4,11 +4,14 @@ from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.test import TestCase
 
+from django.http import HttpRequest
+from django.contrib.auth import authenticate
+
 from forms_builder.forms.models import Form, STATUS_DRAFT, STATUS_PUBLISHED
 from forms_builder.forms.fields import NAMES
 from forms_builder.forms.settings import USE_SITES
 from forms_builder.forms.signals import form_invalid, form_valid
-
+from forms_builder.forms.views import form_detail
 
 current_site = None
 if USE_SITES:
@@ -22,8 +25,11 @@ class Tests(TestCase):
         Simple 200 status check against rendering and posting to forms with
         both optional and required fields.
         """
+        username = "dtest"
+        password = "test"
+        User.objects.create_superuser(username, "", password)
         for required in (True, False):
-            form = Form.objects.create(title="Test", status=STATUS_PUBLISHED)
+            form = Form.objects.create(title="Test", slug="test", status=STATUS_PUBLISHED)
             if USE_SITES:
                 form.sites.add(current_site)
                 form.save()
@@ -35,7 +41,16 @@ class Tests(TestCase):
             fields = form.fields.visible()
             data = dict([("field_%s" % f.id, "test") for f in fields])
             response = self.client.post(form.get_absolute_url(), data=data)
+            # "View on site" submit button redirects back to admin/forms/form page
+            self.assertEqual(response.status_code, 302)
+            # So submit the form directly to get the 200 response
+            req = HttpRequest()
+            req.method = "POST"
+            req.user = authenticate(username=username, password=password)
+            response = form_detail(req, "test")
             self.assertEqual(response.status_code, 200)
+            #
+            form.delete()
 
     def test_draft_form(self):
         """
@@ -65,13 +80,24 @@ class Tests(TestCase):
         form_invalid.connect(invalid)
         valid = lambda **kwargs: events.remove("valid")
         form_valid.connect(valid)
-        form = Form.objects.create(title="Signals", status=STATUS_PUBLISHED)
+        form = Form.objects.create(title="Signals", slug='signals', status=STATUS_PUBLISHED)
         if USE_SITES:
             form.sites.add(current_site)
             form.save()
         form.fields.create(label="field", field_type=NAMES[0][0],
                            required=True, visible=True)
-        self.client.post(form.get_absolute_url(), data={})
+        #
+        # Create an HttpRequest object that can be passed directly to a view function,
+        # specifically to form_detail() which accepts form POST data
+        username = "test"
+        password = "test"
+        User.objects.create_superuser(username, "", password)
+        req = HttpRequest()
+        req.method = "POST"
+        req.user = authenticate(username=username, password=password)
+        #
+        form_detail(req, "signals")
         data = {"field_%s" % form.fields.visible()[0].id: "test"}
-        self.client.post(form.get_absolute_url(), data=data)
+        req.POST.update(data)
+        form_detail(req, "signals")
         self.assertEqual(len(events), 0)
